31b59d70de653963b922a8cb789d6f67bf4f51b1
Giorgi Burduli <giorgiburduli013@gmail.com>
2023-12-12 19:08:56 +0400

    Update rock-paper-scissors.js

--- main.js/rock-paper-scissors.js
+++ main.js/rock-paper-scissors.js
@@ -63,8 +63,8 @@ let result = '';
     document.querySelector('.js-result').innerHTML = result;
 
     document.querySelector('.js-moves').innerHTML = `You
-     <img src="images/${playerMove}.png" class="move-icon" >
-    <img src="images/${computerMove}.png" class="move-icon"> Computer`;
+     <img src="../images/${playerMove}.png" class="move-icon" >
+    <img src="../images/${computerMove}.png" class="move-icon"> Computer`;
 
 
 }
