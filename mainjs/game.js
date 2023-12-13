eba7963d565d36925743285ef647c437f9c9f789
Giorgi Burduli <giorgiburduli013@gmail.com>
2023-12-13 14:13:09 +0400

    Update game.js

--- mainjs/game.js
+++ mainjs/game.js
@@ -63,7 +63,7 @@ let result = '';
     document.querySelector('.js-result').innerHTML = result;
 
     document.querySelector('.js-moves').innerHTML = `You
-     <img src="../images/${playerMove}.png" class="move-icon" >
+    <img src="../images/${playerMove}.png" class="move-icon" >
     <img src="../images/${computerMove}.png" class="move-icon"> Computer`;
 
 
